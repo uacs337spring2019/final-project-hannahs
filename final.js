dabaf0df734d02c0c4de88c8463aabf7b45ca607
hannah smith <hannahhsmith12@email.arizona.edu>
2019-04-24 15:20:13 -0700

    added package.json, app.json

---
+++ final.js
@@ -0,0 +1,500 @@
+"use strict";
+
+(function () {
+	let PORT = process.env.PORT;
+	window.onload = function () {
+		// homepage: 
+		callAjax("listinfo", "none");
+
+		// when "submit new review" button is clicked:
+		document.getElementById("submitnew").onclick = function () {
+			loadSubmitPage();
+		};
+
+		// make header clickable:
+		document.getElementById("title").onclick = function () {
+			callAjax("listinfo", "none");
+		}
+	};
+
+	function callAjax(mode, place) {
+		// place will be the directory name
+		let url = "http://hannahhsmith12.herokuapp.com:" + PORT + "?mode="+mode+"&place="+place;
+		fetch(url)
+			.then(checkStatus)
+			.then(function (responseText) {
+				if (mode === "listinfo") {
+					loadHomepage(responseText);
+				} else if (mode === "reviews") {
+					loadReviews(responseText);
+				} else if (mode === "moreinfo") {
+					loadMoreInfo(responseText);
+				} else if (mode === "map") {
+					initMap(responseText);
+				}
+			})
+			.catch(function (error) {
+				console.log(error);
+			})
+	}
+
+	// loads the clickable list on the homepage
+	function loadHomepage(responseText) {
+		document.getElementById("viewreviews").style.display = "none"
+		document.getElementById("reviewform").style.display = "none"
+		document.getElementById("homepage").style.display = "block";
+		document.getElementById("map").innerHTML = '';
+		document.getElementById("places").innerHTML = '';
+
+		let json = JSON.parse(responseText);
+		let places = json.places;
+
+		callAjax("map", "none");
+
+		for (let i = 0; i < places.length; i ++) {
+			// Load image:
+			let img = document.createElement("img");
+			img.src = "places/"+ places[i].folder + "/img.jpg";
+
+			// Load text for each individual place listing:
+			let h3 = document.createElement("h3");
+			h3.innerHTML = places[i].name;
+			let p1 = document.createElement("p");
+			p1.innerHTML = places[i].address;
+			let rateDiv = document.createElement("div");
+			rateDiv.id = "starDiv";
+			let rating = places[i].rating;
+			for (let i = 0; i < rating; i ++) {
+				let star = document.createElement("img");
+				star.className = "homeStar";
+				star.src = "star.png";
+				rateDiv.appendChild(star);
+			}
+			
+
+			let div = document.createElement("div");
+			div.className = "place";
+			div.id = places[i].folder;
+			div.appendChild(img);
+			div.appendChild(h3);
+			div.appendChild(p1);
+			div.appendChild(rateDiv);
+			div.onclick = function () {
+				callAjax("moreinfo", div.id);
+			}
+			document.getElementById('places').appendChild(div);
+		}
+	}
+
+	function initMap(responseText) {
+		// Process JSON:
+		let json = JSON.parse(responseText);
+		let places = json.places
+
+		// Init map:
+		let platform = new H.service.Platform({
+			'app_id':'btIS6iT6IuLNur7a89Lt',
+			'app_code':'9TqWkeJCx_coB4cnjqvc6A'
+		});
+		let defaultLayers = platform.createDefaultLayers();
+		let map = new H.Map(
+			document.getElementById('map'),
+			defaultLayers.normal.map, 
+			{
+				zoom: 10,
+	         	center: { lat: 32.2226, lng: -110.9747 }
+	    });
+	    // make map interactive
+		var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+		// Create the default UI:
+		var ui = H.ui.UI.createDefault(map, defaultLayers, 'en-US');
+
+		// Container for marker info:
+		var logContainer = document.createElement('ul');
+					logContainer.className ='log';
+					//logContainer.innerHTML = '<li class="log-entry">Try clicking on elements</li>';
+					map.getElement().appendChild(logContainer);
+
+		// Add markers to map:
+		for (let i = 0; i < places.length; i ++) {
+			let geocoder = platform.getGeocodingService(),
+			geocodingParameters = {
+				searchText: places[i].address,
+				jsonattributes: 1
+			};
+
+			geocoder.geocode(
+			geocodingParameters,
+			// on success:
+			function (result) {
+				let locations = result.response.view[0].result[0].location;
+				let marker = new H.map.Marker({lat: locations.displayPosition.latitude, 
+									   lng: locations.displayPosition.longitude});
+				marker.addEventListener('pointerenter', function () {
+				  	let entry = document.createElement('li');
+				  	let br = document.createElement("br");
+				  	entry.className = 'log-entry';
+				  	entry.innerHTML = places[i].name;
+				  	entry.appendChild(br);
+				  	entry.innerHTML += places[i].address;
+				  	entry.appendChild(br);
+				  	entry.innerHTML += "Rating: " + places[i].rating + " stars";
+				  	logContainer.insertBefore(entry, logContainer.firstChild);
+				});
+				marker.addEventListener('pointerleave', function () {
+					logContainer.innerHTML = "";
+				});
+				marker.addEventListener('tap', function () {
+					console.log("clicked");
+					callAjax("moreinfo", places[i].folder);
+				});
+				map.addObject(marker);
+			},
+			// on error:
+			function (error) {
+				console.log(error);
+			}
+			);
+		}
+	}
+
+	function loadSubmitPage() {
+		document.getElementById("homepage").style.display = "none";
+		document.getElementById("reviewform").style.display = "block";
+		document.getElementById("viewreviews").style.display = "none";
+		document.getElementById("submitform").onclick = function () {
+			postReview();
+		};
+	}
+
+	function postReview() {
+		// Retrieve all values from form:
+		console.log("retrieving form info");
+		let name = document.getElementById("name").value;
+
+		let types = document.getElementsByName("type");
+		let type;
+		for (let i = 0; i < types.length; i ++) {
+			if (types[i].checked) {
+				type = types[i].value;
+				break;
+			}
+		}
+		let address = document.getElementById("address").value;
+		let rating = document.getElementById("rating").value;
+		let hours = document.getElementById("hours").value;
+		let styles = document.getElementsByName("style");
+		let style;
+		for (let i = 0; i < styles.length; i ++) {
+			if (styles[i].checked) {
+				style = styles[i].value;
+				break;
+			}
+		}
+		let customers = document.getElementsByName("customer");
+		let customer;
+		for (let i = 0; i < customers.length; i ++) {
+			if (customers[i].checked) {
+				customer = customers[i].value;
+				break;
+			}
+		}
+		let babies = document.getElementsByName("baby");
+		let baby;
+		for (let i = 0; i < babies.length; i ++) {
+			if (babies[i].checked) {
+				baby = babies[i].value;
+				break;
+			}
+		}
+		let unisexes = document.getElementsByName("unisex");
+		let unisex;
+		for (let i = 0; i < unisexes.length; i ++) {
+			if (unisexes[i].checked) {
+				unisex = unisexes[i].value;
+				break;
+			}
+		}
+		let wheelchairs = document.getElementsByName("wheelchair");
+		let wheelchair;
+		for (let i = 0; i < wheelchairs.length; i ++) {
+			if (wheelchairs[i].checked) {
+				wheelchair = wheelchairs[i].value;
+				break;
+			}
+		}
+		let families = document.getElementsByName("family");
+		let family;
+		for (let i = 0; i < families.length; i ++) {
+			if (families[i].checked) {
+				family = families[i].value;
+				break;
+			}
+		}
+		let comment = document.getElementById("comment").value;
+		console.log("comment: "+comment);
+
+		let first = document.getElementById("firstname").value;
+		let last = document.getElementById("lastname").value;
+
+		let folder = getFolderName(name);
+
+		const info = {"folder" : folder, "name" : name, "address" : address, "type": type,
+		"rating" : rating, "hours" : hours, "style" : style, "CO" : customers, 
+		"baby" : baby, "unisex" : unisex, "wheelchair" : wheelchair, "family" : family,
+		"review" : comment, "first" : first, "last" : last};
+		console.log(info);
+
+		const fetchOptions = {
+			method : 'POST',
+			headers : {
+				'Accept' : 'application/json',
+				'Content-Type' : 'application/json'
+			},
+			body : JSON.stringify(info)
+		};
+
+		let url = "http://hannahhsmith12.herokuapp.com:" + PORT + "?";
+		fetch(url, fetchOptions)
+			.then(checkStatus)
+			.then(function(responseText) {
+				document.getElementById("response").innerHTML = responseText;
+				let home = document.createElement("button");
+				home.innerHTML = "Return Home";
+				home.type = "button";
+				document.getElementById("reviewform").appendChild(home);
+				home.onclick = function () {
+					document.getElementById("reviewform").style.display = "none";
+					document.getElementById("homepage").style.display = "block";
+					// reload homepage:
+					document.getElementById("places").innerHTML = "";
+					callAjax("listinfo", "none");
+				}
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+	}
+
+	/**
+		addReview() -- This function adds a review for the specified establishment
+		whose page the button is clicked on.
+	*/
+	function addReview(directory) {
+
+	}
+
+	// folder name is first 8 characters of the establishment name, excluding whitespace
+	function getFolderName(name) {
+		let folder = "";
+		let count = 0, i = 0;
+		while (count < 8 && i < name.length) {
+			if (name[i] != " ") {
+				folder += name[i].toLowerCase();
+				count ++;
+			}
+			i ++;
+		}
+		// let words = name.split(' ');
+		// let folder = "";
+		// for (let i = 0; i < words.length; i ++) {
+		// 	folder += words[i].toLowerCase();
+		// }
+		console.log("folderName: "+folder);
+		return folder;
+	}
+
+	// loads review part of individual review/moreinfo pages:
+	function loadReviews(responseText) {
+		let json = JSON.parse(responseText);
+		let reviews = json.reviews;
+		for (let i = 0; i < reviews.length; i ++) {
+			let div = document.createElement("div");
+
+			let name = document.createElement("div");
+			name.className = "name";
+			name.innerHTML = reviews[i].name;
+			div.appendChild(name);
+
+			let rating = document.createElement("div");
+			rating.className = "indivRating";
+			rating.innerHTML = reviews[i].rating;
+			div.appendChild(rating);
+
+			let review = document.createElement("div");
+			review.className = "review";
+			review.innerHTML = reviews[i].review;
+			div.appendChild(review);
+
+			document.getElementById("reviews").appendChild(div);
+
+			document.getElementById("addreview").onclick = function () {
+				loadSubmitPage();
+			}
+		}
+	}
+
+	function loadMoreInfo(responseText) {
+		console.log("fetching reviews & more info");
+		console.log();
+		// hide other stuff:
+		document.getElementById("homepage").style.display = "none";
+		document.getElementById("viewreviews").style.display = "block";
+
+		let info = JSON.parse(responseText);
+
+		let textDiv = document.createElement("div");
+		textDiv.id = "text";
+		let rightDiv = document.getElementById("rightdiv");		// right side of page
+		let leftDiv = document.getElementById("leftdiv");
+		
+		// FORMAT: 
+		// {"folder" : directory, "name" : lines[0], "address" : lines[1], "rating" : lines[2],
+		// "hours" : lines[3], "style" : lines[4], "CO" : lines[5], "baby" : lines[6], "unisex" : lines[7], 
+		// "wheelchair" : lines[8], "family" : lines[9]}
+
+		// RATING:
+		let rating = document.createElement("div");
+		rating.className = "rating";
+		rating.innerHTML = "Rating: ";
+		let num = info.rating;
+		for (let i = 0; i < num; i ++) {
+			let star = document.createElement("img");
+			star.src = "star.png";
+			rating.appendChild(star);
+		}
+		rightDiv.appendChild(rating);
+
+		// PICTURE:
+		let img = document.createElement("img");
+		img.src = "places/" + info.folder + "/img.jpg";
+		rightDiv.appendChild(img);
+
+		// HEADING:
+		let h2 = document.createElement("h2");
+		h2.innerHTML = info.name;
+		leftDiv.appendChild(h2);
+
+		// ESTABLISHMENT TYPE:
+		let type = document.createElement("div");
+		type.className = "type";
+		type.innerHTML = info.type;
+		leftDiv.appendChild(type);
+
+		// ADDRESS:
+		let address = document.createElement("div");
+		address.className = "address";
+		address.innerHTML = info.address;
+		leftDiv.appendChild(address);
+
+		// HOURS:
+		let hours = document.createElement("div");
+		hours.className = "hours";
+		hours.innerHTML =  "Hours: " + info.hours;
+		leftDiv.appendChild(hours);
+
+		// STALLS OR SINGLE:
+		let style = document.createElement("div");
+		let stallsimg = document.createElement("img");
+		let singleimg = document.createElement("img");
+		style.className += "style";
+		style.innerHTML = "Stalls: ";
+		if (info.style === "stalls") {
+			stallsimg.src = "check.png";
+		} else {
+			stallsimg.src = "notcheck.png";
+		}
+		style.appendChild(stallsimg);
+		style.innerHTML += "	Single: ";
+		if (info.style === "single") {
+			singleimg.src = "check.png";
+		} else {
+			singleimg.src = "notcheck.png";
+		}
+		style.appendChild(singleimg);
+		textDiv.appendChild(style);
+
+		// CUSTOMERS ONLY:
+		let customers = document.createElement("div");
+		customers.className = "customers";
+		customers.innerHTML = "For use by customers only? "
+		let custimg = document.createElement("img");
+		if (info.CO === "true") {
+			custimg.src = "check.png";
+		} else {
+			custimg.src = "notcheck.png";
+		}
+		customers.appendChild(custimg);
+		textDiv.appendChild(customers);
+
+		// BABIES:
+		let baby = document.createElement("div");
+		baby.className = "baby";
+		baby.innerHTML = "Changing table? ";
+		let babyimg = document.createElement("img");
+		if (info.baby === "true") {
+			babyimg.src = "check.png";
+		} else {
+			babyimg.src = "notcheck.png";
+		}
+		baby.appendChild(babyimg);
+		textDiv.appendChild(baby);
+
+		// UNISEX:
+		let unisex = document.createElement("div");
+		unisex.className = "unisex";
+		unisex.innerHTML = "Unisex option? ";
+		let uniseximg = document.createElement("img");
+		if (info.unisex === "true") {
+			uniseximg.src = "check.png";
+		} else {
+			uniseximg.src = "notcheck.png";
+		}
+		unisex.appendChild(uniseximg);
+		textDiv.appendChild(unisex);
+
+		// WHEELCHAIR:
+		let wheelchair = document.createElement("div");
+		wheelchair.className = "wheelchair";
+		wheelchair.innerHTML = "Wheelchair accessibility? ";
+		let wheelimg = document.createElement("img");
+		if (info.wheelchair === "true") {
+			wheelimg.src = "check.png";
+		} else {
+			wheelimg.src = "notcheck.png";
+		}
+		wheelchair.appendChild(wheelimg);
+		textDiv.appendChild(wheelchair);
+
+		// FAMILY:
+		let family = document.createElement("div");
+		family.className = "family";
+		family.innerHTML = "Family restroom? ";
+		let famimg = document.createElement("img");
+		if (info.family === "true") {
+			famimg.src = "check.png";
+		} else {
+			famimg.src = "notcheck.png";
+		}
+		family.appendChild(famimg);
+		textDiv.appendChild(family);
+
+		leftDiv.appendChild(textDiv);
+
+		// Load reviews:
+		callAjax("reviews", info.folder);
+	}
+
+	function checkStatus(response) {
+		if (response.status >= 200 && response.status < 300) {
+			return response.text();
+		} else if (response.status === 404) {
+			return Promise.reject(new Error(response.status));
+		} else if (response.status === 410) {
+			return Promise.reject(new Error(response.status));
+		} else {
+			return Promise.reject(new Error(response.status));
+		}
+	}
+
+}) ();
---
+++ final_service.js
@@ -0,0 +1,182 @@
+// FILE FORMAT:
+// 		name
+//		location/address
+// 		cleanliness (out of 10)
+//		hours
+//		stalls vs single
+//		"by customers only"
+//		baby accomadations
+//		unisex option
+//		wheelchair accessibility
+//		family restroom
+
+"use strict";
+
+const express = require("express");
+const app = express();
+
+const fs = require('fs');
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json(); 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", 
+               "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+app.use(express.static('public'));
+
+/**
+	readFile(folder) : This function opens a file and returns it's contents
+	as a string. Throws an error if unable to open file.
+*/
+function readFile(fileName) {
+	let file;
+	try {
+		file = fs.readFileSync(fileName, 'utf8');
+	} catch(e) {
+		console.log('Error: ', e.stack);
+	}
+	return file;
+}
+
+/**
+	getBasic(directory) : This function reads the info.txt file in the specified 
+	folder and returns the first 3 lines as a JSON object.
+*/
+function getBasic(directory) {
+	let basic = readFile("places/" + directory + "/info.txt");
+	let lines = basic.split('\n');
+	let place = {"folder" : directory, "name" : lines[0], "address" : lines[1], "type" : lines[2],
+		"rating" : lines[3]}
+	return place;
+}
+
+/**
+	getInfo(directory) : This function reads the info.txt file in the specified 
+	folder and returns the contents as a JSON object.
+*/
+function getInfo(directory) {
+	let info = readFile("places/"+directory+"/info.txt");
+	let lines = info.split('\n');
+	let place = {"folder" : directory, "name" : lines[0], "address" : lines[1], "type": lines[2],
+		"rating" : lines[3], "hours" : lines[4], "style" : lines[5], "CO" : lines[6], 
+		"baby" : lines[7], "unisex" : lines[8], "wheelchair" : lines[9], "family" : lines[10]};
+	// style = stalls or single
+	// CO = customers only
+	return place;
+}
+
+function getReviews(directory) {
+	let file = readFile("places/" + directory + "/reviews.txt");
+	let lines = file.split('\n');
+	let reviews = [];
+	for (let i = 0; i < lines.length; i ++) {
+		let info = lines[i].split(':::');
+		reviews[i] = {"folder" : directory, "name": info[0], "rating": info[1], "review": info[2]};
+	}
+	return {reviews};
+}
+
+function getAllBasic() {
+	let folders = fs.readdirSync("places");
+	let places = [];
+	for (let i = 1; i < folders.length; i ++) {
+		let info = getBasic(folders[i]);
+		places[i-1] = info;
+	}
+	return places;
+}
+
+// GET:
+console.log('web service started');
+app.get('/', function (req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+
+	// get query parameters:
+	let mode = req.query.mode;
+	let location = req.query.place;
+
+	//let folders = fs.readdirSync("places");
+	//console.log(files);
+	if (mode === "listinfo") {
+		console.log("listinfo");
+		let places = [];
+		let directories = fs.readdirSync("places");
+		for (let i = 1; i < directories.length; i ++) {
+			let place = getBasic(directories[i]);
+			places[i-1] = place;
+		}
+		places = {places};
+		console.log(places);
+		res.send(JSON.stringify(places));
+	} else if (mode === "moreinfo") {
+		let info = getInfo(location);
+		res.send(JSON.stringify(info));
+	} else if (mode === "reviews") {
+		let reviews = getReviews(location);
+		console.log(reviews);
+		res.send(JSON.stringify(reviews));
+	} else if (mode === "map") {
+		let places = getAllBasic();
+		console.log("addresses: "+places);
+		res.send(JSON.stringify({places}));
+	}
+});
+
+// POST:
+app.post('/', jsonParser, function (req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+	let folder = req.body.folder;
+	let name = req.body.name;
+	let address = req.body.address;
+	let type = req.body.type;
+	let rating = req.body.rating;
+	let hours = req.body.hours;
+	let style = req.body.style;
+	let customers = req.body.customers;
+	let baby = req.body.baby;
+	let unisex = req.body.unisex;
+	let wheelchair = req.body.wheelchair;
+	let family = req.body.family;
+	let comment = req.body.comment;
+	let first = req.body.first;
+	let last = req.body.last;
+
+	let info = name + "\n" + address + "\n" + type + "\n" + rating + "\n" +
+		hours + "\n" + style + "\n" + customers + "\n" + baby + "\n" + unisex
+		+ "\n" + wheelchair + "\n" + family;
+
+	let dir = process.cwd() + "/places/" + folder;
+	if (!fs.existsSync(dir)) {
+		fs.mkdirSync(dir);
+	}
+
+	// write info file:
+	fs.writeFile(process.cwd() + "/places/" + folder + "/info.txt", info, function(err) {
+		if(err) {
+			console.log(err);
+			res.status(400);
+			res.send('Error submitting review.');
+		}
+	});
+
+	// write review file:
+	let review = first + last[0] + ".:::" + rating + ":::" + comment;
+	fs.appendFile(process.cwd() + "/places/" + folder + "/reviews.txt", review, function(err) {
+		if(err) {
+			console.log(err);
+			res.status(400);
+			console.log("error");
+			res.send('Error submitting review.');
+		}
+		console.log("success");
+		res.send('Review successfully submitted!');
+	});
+
+});
+
+app.listen(process.env.PORT);
